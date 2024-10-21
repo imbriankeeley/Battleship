05f45f44e8dab3020b087508c75cedbc6fcb8775
Brian Keeley <imbriankeeley@gmail.com>
2024-10-21 10:36:26 -0400

    feat: added gameboard logic to render gameboard and update it

--- src/Dom.js
+++ src/Dom.js
@@ -1,3 +1,35 @@
+import { Gameboard } from './Gameboard';
+
+export const domUserBoard = document.getElementById('user-board');
+export const domComputerBoard = document.getElementById('computer-board');
+
 export class Dom {
-    
+	static renderBoard(board, domBoard) {
+		while (domBoard.firstChild) {
+			domBoard.removeChild(domBoard.firstChild);
+		}
+
+		for (let i = 0; i < 10; i++) {
+			let y = i;
+			for (let j = 0; j < 10; j++) {
+				let x = j;
+
+				const cell = document.createElement('div');
+				cell.className = 'cell';
+				const hit = document.createElement('p');
+				hit.textContent = 'X';
+				hit.className = 'hit';
+				const ship = document.createElement('p');
+				ship.textContent = 'O';
+				ship.className = 'ship';
+				domBoard.appendChild(cell);
+
+				if (typeof board.matrix[y][x] === 'object') {
+					board.matrix[y][x].hit == true
+						? cell.append(hit)
+						: cell.append(ship);
+				}
+			}
+		}
+	}
 }
--- src/UserInterface.js
+++ src/UserInterface.js
@@ -1,19 +1,24 @@
+import { Dom, domUserBoard, domComputerBoard } from './Dom';
+import { Gameboard } from './Gameboard';
+import { Ship } from './Ship';
+import { Player } from './Player';
+import { LocalStorage } from './LocalStorage';
+
 export class UserInterface {
-    static start() {
-        document.addEventListener('DOMContentLoaded', () => {
-            const userBoard = document.getElementById('user-board');
-            const computerBoard = document.getElementById('computer-board');
-        
-            function createBoard(board) {
-                for (let i = 0; i < 100; i++) {
-                    const cell = document.createElement('div');
-                    board.appendChild(cell);
-                }
-            }
-        
-            createBoard(userBoard);
-            createBoard(computerBoard);
-        });
-        
-    }
+	static start() {
+		document.addEventListener('DOMContentLoaded', () => {
+			const userGameboard = new Gameboard();
+			const computerGameboard = new Gameboard();
+
+			Dom.renderBoard(userGameboard, domUserBoard);
+			Dom.renderBoard(computerGameboard, domComputerBoard);
+
+			const userShip1 = new Ship(3);
+			userGameboard.placeShip(userShip1, [0, 0]);
+			userGameboard.placeShip(userShip1, [0, 1]);
+			userGameboard.placeShip(userShip1, [0, 2]);
+			userGameboard.receiveAttack([0, 1]);
+			Dom.renderBoard(userGameboard, domUserBoard);
+		});
+	}
 }
